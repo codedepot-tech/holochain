23277543a6b22ea25d502e8a18d3429d15938e85
Stefan Junker <mail@stefanjunker.de>
2021-08-19 12:30:50 +0200

    bump holonix for more recent rust version

--- config.nix
+++ config.nix
@@ -15,14 +15,14 @@
 
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "014d28000c8ed021eb84000edfe260c22e90af8c";
+   ref = "3ea3d4c7e2ab8f8bce86b19b79590aecaedd985e";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
    #       the sha here changes (the sha is the cache key for downloads)
    # note: to get a new sha, get nix to try and download a bad sha
    #       it will complain and tell you the right sha
-   sha256 = "0hl5xxxjg2a6ymr44rf5dfvsb0c33dq4s6vibva6yb76yvl6gwfi";
+   sha256 = "19ncf7jsw5nkn8rsvp8bl2m0hijy0vq10ximxs0lksah013vg2si";
 
    # the github owner of the holonix repo
    owner = "holochain";
